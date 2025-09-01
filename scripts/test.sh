49dd7c252a3fc9df58c940d3b654a7d66885a247
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2025-09-01 16:47:49 +0100

    non existent tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,8 +6,6 @@ source ./scripts/install.sh
 # c# tests
 dotnet test ./tests/Unit/Domain.Tests/Domain.Tests.csproj
 dotnet test ./tests/Unit/Facade.Tests/Facade.Tests.csproj
-dotnet test ./tests/Unit/Messaging.Tests/Messaging.Tests.csproj
-dotnet test ./tests/Unit/Proxy.Tests/Proxy.Tests.csproj
 dotnet test ./tests/Integration/Integration.Tests/Integration.Tests.csproj
 
 echo $?
