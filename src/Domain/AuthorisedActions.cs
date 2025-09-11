db699ec7f265d1f286ca27e8cabdf418f4e9f5f4
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2025-09-11 14:18:07 +0100

    tidy

--- src/Domain/AuthorisedActions.cs
+++ src/Domain/AuthorisedActions.cs
@@ -6,10 +6,6 @@ namespace Linn.Portal.Authorization.Domain
 
         public const string CreatePermission = "permissions:create";
 
-        // special admin privilege
-        // grants the power to give other subjects the power to create permissions with arbitrary association
-        // as such is not scoped to any association, and so is a sort of system level privilege
-        // its scopeType will be "System"
         public const string AuthAdmin = "authorization:admin";
     }
 }
